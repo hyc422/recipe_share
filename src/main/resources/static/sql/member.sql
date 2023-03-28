a015ddcbc6b0e800b88ca0ecfd21071fc5888c33
jaewo0 <120548836+jaewo0@users.noreply.github.com>
2023-03-28 14:46:48 +0900

    CJW #2 sql 수정

--- src/main/resources/static/sql/member.sql
+++ src/main/resources/static/sql/member.sql
@@ -1,13 +1,11 @@
 CREATE TABLE th_member(
-	idx number(10) PRIMARY KEY,		--회원 idx
-	id varchar2(20) NOT NULL,		--회원 id
+	idx number(10) PRIMARY KEY,	--회원 idx
+	email varchar2(20) NOT NULL,	--회원 email
 	password varchar2(20) NOT NULL,	--회원 password
-	name varchar2(20) NOT NULL,	--회원 name
+	name varchar2(20) NOT NULL,		--회원 name
 	nickname varchar2(20) NOT NULL,	--회원 nickname
 	phone varchar2(20) NOT NULL,	--회원 phone
-	email varchar2(20) NOT NULL,	--회원 email
-	birth varchar2(20) NOT NULL,		--회원 birth
-	createAt timestamp DEFAULT sysdate,	--회원 createAt
+	birth varchar2(20) NOT NULL,	--회원 birth
 	grade number(1) DEFAULT '1' 	--회원 grade
 		--0 관리자
 		--1 신규회원 /요린이
@@ -15,3 +13,4 @@ CREATE TABLE th_member(
 		--3 고급회원 /셰프
 		--4 마스터회원 /고든램지
 );
+SELECT * FROM TH_MEMBER tm ;
