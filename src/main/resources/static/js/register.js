ef524a35093dbce06d0316ce35134105d48bad1a
jaewo0 <120548836+jaewo0@users.noreply.github.com>
2023-03-30 15:31:24 +0900

    CJW#2 이메일 인증 기능 구현 완료

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,28 +1,54 @@
 
-const isValid = true;
-
+let isValid = false;
 document.querySelector("#idcheck").addEventListener('click', function() {
-	const id = document.querySelector("#id").value;
+	const id = document.querySelector("#email").value;
 	const xhr = new XMLHttpRequest();
 	xhr.responseType = 'text';
-	xhr.open('GET', '/register/' + id)
+	xhr.open('GET', '/find/' + id)
 	xhr.send()
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    if (xhr.status === 200) {
-      const result = parseInt(xhr.responseText);
-      console.log(result)
-      if (result === 0) {
-        document.querySelector('#idOk').innerHTML = '사용 가능한 아이디 입니다.';
-        document.querySelector('#idOk').style.color = 'green';
-      } else if (result === 1) {
-        document.querySelector('#idOk').innerHTML = '사용 중인 아이디 입니다.';
-        document.querySelector('#idOk').style.color = 'red';
-      }
-    }
-  }
-};
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				const result = parseInt(xhr.responseText);
+				console.log(result)
+				if (result === 0) {
+					document.querySelector('#idOk').innerHTML = '사용 가능한 아이디 입니다.';
+					document.querySelector('#idOk').style.color = 'green';
+					isValid = true;
+				} else if (result === 1) {
+					document.querySelector('#idOk').innerHTML = '사용 중인 아이디 입니다.';
+					document.querySelector('#idOk').style.color = 'red';
+				}
+			}
+		}
+	};
 })
+document.querySelector("#nickchk").addEventListener('click', function() {
+	const nickname = document.querySelector("#nickname").value;
+	const xhr = new XMLHttpRequest();
+	xhr.responseType = 'text';
+	xhr.open('GET', '/finds/' + nickname)
+	xhr.send()
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				const result = parseInt(xhr.responseText);
+				console.log(result)
+				if (result === 0) {
+					document.querySelector('#nnOk').innerHTML = '사용 가능한 아이디 입니다.';
+					document.querySelector('#nnOk').style.color = 'green';
+					isValid = true;
+				} else if (result === 1) {
+					document.querySelector('#nnOk').innerHTML = '사용 중인 아이디 입니다.';
+					document.querySelector('#nnOk').style.color = 'red';
+				}
+			}
+		}
+	};
+})
+
+
+
 
 //비밀번호 검사
 document.querySelector("#password").addEventListener('keyup', function() {
@@ -31,8 +57,8 @@ document.querySelector("#password").addEventListener('keyup', function() {
 	if (!pwdchk.test(pwd)) {
 		document.querySelector('#pwdcheckmsg').innerHTML = '사용 불가능'
 		document.querySelector('#pwdcheckmsg').style.color = 'red'
-		isValid = false;
 	} else {
+		isValid = true;
 		document.querySelector('#pwdcheckmsg').innerHTML = '사용 가능'
 		document.querySelector('#pwdcheckmsg').style.color = 'green'
 	}
@@ -44,15 +70,50 @@ document.querySelector("#password2").addEventListener('keyup', function() {
 	if (pwd != pwd2) {
 		document.querySelector('#pwd2checkmsg').innerHTML = '사용 불가능'
 		document.querySelector('#pwd2checkmsg').style.color = 'red'
-		isValid = false;
 	} else {
+		isValid = true;
 		document.querySelector('#pwd2checkmsg').innerHTML = '사용 가능'
 		document.querySelector('#pwd2checkmsg').style.color = 'green'
 	}
+});
+//이메일 전송
+$(document).ready(function() {
+	$("#send-email-button").click(function() {
+		var email = $("#email").val();
+
+		$.ajax({
+			type: "POST",
+			url: "/api/mail/confirm.json",
+			data: {
+				"email": email
+			},
+			success: function(data) {
+				alert("이메일이 발송되었습니다. 인증코드를 확인해주세요.");
+				document.querySelector('#emailcheck').addEventListener('click', function() {
+				const code = data;
+				const codec = document.querySelector('#verification-codechk').value;
+					if (codec === code) {
+						document.querySelector('#true').innerHTML = '인증완료'
+					document.querySelector('#pwd2checkmsg').style.color = 'green'
+						isValid = true;
+					} else {
+						document.querySelector('#true').innerHTML = '인증실패'
+						document.querySelector('#pwd2checkmsg').style.color = 'red'
+					}
+				})
+			},
+			error: function(data) {
+				alert("이메일 발송에 실패하였습니다.");
+			}
+		});
+	});
 });
 
 document.querySelector('#join').addEventListener('click', function() {
 	if (isValid) {
 		document.join.submit();
+	} else {
+		alert("정보확인요망")
 	}
 })
+
--- src/main/resources/static/sql/member.sql
+++ src/main/resources/static/sql/member.sql
@@ -1,11 +1,11 @@
 CREATE TABLE th_member(
 	idx number(10) PRIMARY KEY,		--회원 idx
-	id varchar2(20) NOT NULL,		--회원 id
+	email varchar2(20) NOT NULL,	--회원 email
 	password varchar2(20) NOT NULL,	--회원 password
 	name varchar2(20) NOT NULL,	--회원 name
 	nickname varchar2(20) NOT NULL,	--회원 nickname
 	phone varchar2(20) NOT NULL,	--회원 phone
-	email varchar2(20) NOT NULL,	--회원 email
+
 	birth varchar2(20) NOT NULL,		--회원 birth
 	createAt timestamp DEFAULT sysdate,	--회원 createAt
 	grade number(1) DEFAULT '1' 	--회원 grade
